8112c767f6806295eb5bb3fd23e44c6086612ab8
Almansur342 <abdullahalmansur2002@gmail.com>
2024-12-04 11:17:41 +0600

    project completed successfully

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Car Store Project');
+});
+
 app.use('/api', CarRoutes);
 app.use('/api', OrderRoutes);
 
